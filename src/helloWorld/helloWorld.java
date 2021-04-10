b2a7dced5ff511b5362163030a79eebd39fdf6df
Marjia <merziaismail@gmail.com>
2021-04-10 19:57:50 +0200

    Marjia edit:Input box

--- src/helloWorld/helloWorld.java
+++ src/helloWorld/helloWorld.java
@@ -1,10 +1,18 @@
+
+
 package helloWorld;
+import java.util.*;
 
 public class helloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Git is Awesome!");
+		System.out.print("Enter a message: ");
+		Scanner input = new Scanner(System.in); 					//Reads the number entered by the user
+		String message = input.nextLine();	
+		System.out.println(message);
+		
 
 	}
 
 }
+	
